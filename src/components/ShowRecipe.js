585e79c8930c193b2593be0ba95f4a374e856248
Willyam Ramos <ramoswillyam1@gmail.com>
2022-07-06 22:57:15 -0700

    New feature showing un used ingredients

--- src/components/ShowRecipe.js
+++ src/components/ShowRecipe.js
@@ -6,15 +6,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const ShowRecipe = ({ recipe }) => {
+
+    const unUsedIngredientsList = () => {
+        if (recipe)
+            return recipe.unusedIngredients.map(ingred => {
+                return ingred.name
+            })
+    };
+
+    const renderUnusedIngredients = () => {
+        if (recipe.unusedIngredients.length < 1) {
+            return null;
+        }
+        return (
+            <div>
+                Missing: {unUsedIngredientsList().join(', ')}
+            </div>
+        )
+    };
+
     const closeRecipe = () => {
         document.querySelector('#modal').style.display = 'none';
-    }
+    };
+
     if (!recipe) {
         return null;
     }
-
     return ReactDOM.createPortal(
         <div id='innerModal'>
+
             <h1>
                 {recipe.title}
             </h1>
@@ -27,7 +47,15 @@ const ShowRecipe = ({ recipe }) => {
 
             </button>
 
-            <img src={recipe.image} alt={recipe.title} />
+            <img
+                src={recipe.image}
+                alt={recipe.title}
+                id='recipeImg'
+            />
+
+            <div id='unUsedIngredients'>
+                {renderUnusedIngredients()}
+            </div>
 
             <MiniIngredientList
                 missedIngredients={recipe.missedIngredients}
