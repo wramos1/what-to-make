dee27ffe93152f9e5965702286f2541e9211d2ec
Willyam Ramos <ramoswillyam1@gmail.com>
2022-07-18 13:23:34 -0700

    Removed header icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,12 @@ const Header = () => {
     return (
         <nav>
             <div id='header'>
-
                 <div>
                     <h1 id='title'>
-                        What To Make...
+                        What to make...
                     </h1>
+                    <span>Struggling to find something to eat?</span>
                 </div>
-
-                <div>
-                    <img
-                        id='foodIcon'
-                        src="https://cdn-icons-png.flaticon.com/512/2927/2927347.png"
-                        alt="foodIcon"
-                    />
-                </div>
-
             </div>
         </nav>
     )
