81549ad79344dbf08ce8faeb11a624cf5228a5e4
Willyam Ramos <ramoswillyam1@gmail.com>
2022-07-18 13:16:42 -0700

    Separated API call statuses in state

--- src/reducers/recipesSlice.js
+++ src/reducers/recipesSlice.js
@@ -1,11 +1,15 @@
 import { createSlice, createAsyncThunk, nanoid } from "@reduxjs/toolkit";
-import recipe from "../apis/recipe";
+import recipe, { getInstructions, getRecipeById } from "../apis/recipe";
 
 
 const initialState = {
     ingredients: [],
     recipes: [],
-    status: '',
+    recipe: {},
+    instructions: [],
+    recipesStatus: '',
+    instructionStatus: '',
+    recipeStatus: '',
     error: null,
     exists: false,
 }
@@ -24,7 +28,19 @@ export const fetchRecipe = createAsyncThunk('recipes/fetchRecipes', async (obj, 
             number: 12
         }
     });
-    return response.data
+    return response.data;
+})
+
+export const fetchInstructions = createAsyncThunk('recipes/fetchInstructions', async (id) => {
+    const response = await getInstructions(id);
+    const mappedData = await response.data.map((res) => res.steps.map((stepObj) => stepObj.step))
+    return mappedData;
+});
+
+export const fetchRecipeById = createAsyncThunk('recipes/fetchRecipeById', async (id) => {
+    const response = await getRecipeById(id);
+
+    return response.data;
 })
 
 const recipesSlice = createSlice({
@@ -58,18 +74,51 @@ const recipesSlice = createSlice({
     },
     extraReducers(builder) {
         builder.addCase(fetchRecipe.pending, (state) => {
-            state.status = 'LOADING';
+            state.recipesStatus = 'LOADING';
         });
         builder.addCase(fetchRecipe.fulfilled, (state, action) => {
-            return { ...state, recipes: action.payload, status: 'SUCCEEDED' };
-        })
+            return { ...state, recipes: action.payload, recipesStatus: 'SUCCEEDED' };
+        });
         builder.addCase(fetchRecipe.rejected, (state, action) => {
-            state.status = 'ERROR'
+            state.recipesStatus = 'ERROR'
             state.error = action.error.message
-        })
+        });
+
+        builder.addCase(fetchInstructions.pending, (state) => {
+            state.instructionStatus = 'LOADING';
+        });
+        builder.addCase(fetchInstructions.fulfilled, (state, action) => {
+            return { ...state, instructions: action.payload, instructionStatus: 'SUCCEEDED' };
+        });
+        builder.addCase(fetchInstructions.rejected, (state, action) => {
+            state.instructionStatus = 'ERROR'
+            state.error = action.error.message
+        });
+
+        builder.addCase(fetchRecipeById.pending, (state) => {
+            state.recipeStatus = 'LOADING';
+        });
+        builder.addCase(fetchRecipeById.fulfilled, (state, action) => {
+            return { ...state, recipe: action.payload, recipeStatus: 'SUCCEEDED' };
+        });
+        builder.addCase(fetchRecipeById.rejected, (state, action) => {
+            state.recipeStatus = 'ERROR'
+            state.error = action.error.message
+        });
+
     }
 });
+export const ifExists = (state) => state.recipes.exists;
 
+export const getRecipeStatus = (state) => state.recipes.recipeStatus;
+
+export const getRecipesStatus = (state) => state.recipes.recipesStatus;
+
+export const getInstructionStatus = (state) => state.recipes.instructionStatus;
+
+export const getRecipe = (state) => state.recipes.recipe;
+
+export const getAllSteps = (state) => state.recipes.instructions;
 
 export const getAllIngredients = (state) => state.recipes.ingredients;
 
